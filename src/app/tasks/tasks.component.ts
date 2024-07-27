e4d533ea6d33906d9a54e1efde38445f76b5e83c
elaheh farokhi <farokhi@ptcvep.net>
2024-07-27 08:21:38 +0330

    first commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from './dummy-tasks';
+import { Task } from "./Task";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) username?: string;
+  @Input({ required: true }) userId?: string;
+  tasks:Task[] = DUMMY_TASKS;
+
+  get selectedUserTasks(){
+    return this.tasks.filter(p => p.userId === this.userId);
+  }
+
+  onAddTask(){
+  }
+}
