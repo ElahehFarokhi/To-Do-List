2c13a60684cdc4bdac27064a98232b7595a253ad
elaheh farokhi <farokhi@ptcvep.net>
2024-07-27 09:37:36 +0330

    Add New-Task Component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,27 +2,35 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
 import { Task } from './task.model';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) username?: string;
   @Input({ required: true }) userId?: string;
-
+isAddingTask = false;
   tasks: Task[] = DUMMY_TASKS;
 
   get selectedUserTasks() {
     return this.tasks.filter((p) => p.userId === this.userId);
   }
 
-  onAddTask() {}
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((p) => p.id != id);
   }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+  
 }
